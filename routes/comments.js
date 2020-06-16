a747ccb9c38f966cd7e9436419dd579312f42369
pranjal <pranjalbhagade930@gmail.com>
2020-06-16 09:58:52 +0000

    initial deploy commit

---
+++ routes/comments.js
@@ -0,0 +1,73 @@
+var express = require("express"),
+	router  = express.Router({mergeParams:true}),
+	Campground = require("../models/campground"),
+	Comment = require("../models/comment"),
+	middleware = require("../middleware");
+
+router.get("/new",middleware.isLoggedIn,function(req,res){
+	Campground.findById(req.params.id,function(err,campground){
+		if(err){
+			console.log(err);
+		}else{
+			res.render("comments/new",{campground:campground});
+		}
+	})
+});
+
+router.post("/",middleware.isLoggedIn,function(req,res){
+	Campground.findById(req.params.id,function(err,campground){
+		if(err){
+			console.log(err);	
+		}else{
+			Comment.create(req.body.comment,function(err,comment){
+				if(err)
+					console.log(err);
+				else
+					{
+						comment.author.id=req.user._id;
+						comment.author.username=req.user.username;
+						comment.save();
+						campground.comments.push(comment);
+						campground.save();
+						res.redirect("/campgrounds/"+campground._id);
+						
+					}
+			})
+		}				
+	});
+	
+})
+
+router.get("/:comment_id/edit",middleware.checkCommentOwnership,function(req,res){
+	Comment.findById(req.params.comment_id,function(err,comment){
+		if(err)
+			console.log(err);
+		else
+			{
+				res.render("comments/edit",{campground_id:req.params.id,comment:comment});
+			}
+	});
+	
+})
+
+router.put("/:comment_id",middleware.checkCommentOwnership,function(req,res){
+	Comment.findByIdAndUpdate(req.params.comment_id,req.body.comment,function(err){
+		if(err)
+			console.log(err);
+		else
+			res.redirect("/campgrounds/"+req.params.id);
+	})
+})
+
+router.delete("/:comment_id",middleware.checkCommentOwnership,function(req,res){
+	Comment.findByIdAndRemove(req.params.comment_id,function(err){
+		if(err)
+			console.log(err);
+		else
+		{
+			res.redirect("/campgrounds/"+req.params.id);
+		}
+	})
+})
+
+module.exports=router;
---
+++ routes/index.js
@@ -0,0 +1,56 @@
+var express = require("express"),
+	router  = express.Router(),
+	passport = require("passport"),
+	User     = require("../models/user");
+	
+
+router.get("/",function(req,res){
+	res.render("landing");
+	
+});
+
+
+router.get("/register",function(req,res){
+	res.render("register");
+})
+
+router.post("/register",function(req,res){
+	var newUser= new User({username:req.body.username});
+	User.register(newUser,req.body.password,function(err,user){
+		if(err){
+			req.flash("error","username unavailable");
+			return res.redirect("/register");
+		}
+		passport.authenticate("local")(req,res,function(){
+			res.redirect("/campgrounds");
+		});
+	});
+});
+
+router.get("/login",function(req,res){
+	res.render("login");
+})
+
+router.post("/login",passport.authenticate("local",{
+		successRedirect:"/campgrounds",
+		failureRedirect:"/login"
+	}),function(req,res){
+	req.flash("error","incorrect username or password");
+	res.redirect("/login");
+})
+
+
+router.get("/logout",function(req,res){
+	req.logout();
+	req.flash("success","Logged out successfully");
+	res.redirect("/campgrounds");
+})
+
+function isLoggedIn(req,res,next){
+	if (req.isAuthenticated()){
+		return next();
+	}
+	res.redirect("/login");
+}
+
+module.exports=router;
