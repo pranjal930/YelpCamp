1b82de48019bcb56363c2b1ced8247c9f653dfee
pranjal <pranjalbhagade930@gmail.com>
2020-06-16 12:11:02 +0000

    added mongodb support

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ seedDB        = require("./seeds")
 var commentRoutes    = require("./routes/comments"),
 	campgroundRoutes = require("./routes/campgrounds"),
     indexRoutes       = require("./routes/index")
-mongoose.connect("mongodb://localhost:27017/yelp_camp",{useNewUrlParser:true});
+mongoose.connect("mongodb+srv://devsprout:7507458931@cluster0-h2xdt.mongodb.net/yelp_camp?retryWrites=true&w=majority",{useNewUrlParser:true});
 app.use(bodyParser.urlencoded({extended:true}));
 
 app.set("view engine","ejs");
